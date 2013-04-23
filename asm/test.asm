6bd71c967fb0fb53994cbcaa4af1dd817b3f18ae
Dan Ellis <dan@halftreelabs.com>
2013-04-23 03:06:40 -0400

    Add vector table

--- asm/test.asm
+++ asm/test.asm
@@ -1,5 +1,20 @@
-start:
-mov r0, #10
-subs r0, r0, #1
-bne start
-swi 1
+.text 
+.code 32 
+
+    b reset // Reset
+    b . // Undefined instruction
+    b . // Software interrupt
+    b . // Prefetch abort
+    b . // Data abort
+    b . // Reserved
+    b . // IRQ
+    b . // FIQ
+
+.string "Armem test binary"
+.align 4
+
+reset:
+    mov r0, #10
+    subs r0, r0, #1
+    bne reset
+    swi 1
